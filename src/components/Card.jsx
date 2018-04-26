93c33fb3a458947b3f3d92bbb26ae7938a018771
r-lee1 <13248883+r-lee1@users.noreply.github.com>
2018-04-26 00:41:27 -0700

    add card modal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React from 'react';
+import Modal from 'react-modal';
+import CardModal from './CardModal';
+
+Modal.setAppElement('#root');
 
 class Card extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      modalIsOpen: false
+    };
+
+    this.openModal = this.openModal.bind(this);
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+
+  openModal() {
+    this.setState({modalIsOpen: true});
+  }
+
+  afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    this.subtitle.style.color = '#f00';
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
   render() {
     console.log(this.props.characterData);
     return (
-      <div className="charBlock">
+      <div onClick={this.openModal} className="charBlock">
         <img className="charImage" src={`${this.props.characterData.thumbnail.path}/standard_xlarge.jpg`} />
         <h2 className="charName">{this.props.characterData.name}</h2>
+        <Modal isOpen={this.state.modalIsOpen}>
+          <CardModal props={this.state}/>
+        </Modal>
       </div>
     );
   }
---
+++ src/components/CardModal.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class CardModal extends React.Component {
+  render() {
+    console.log(this.props);
+    return(
+        <h1>hello</h1>
+    );
+  }
+}
+
+export default CardModal;
